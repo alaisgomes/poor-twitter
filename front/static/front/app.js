d33897776701379082226b26d0282435698fdd1c
Aline Tavares <alaisgomes@gmail.com>
2019-06-11 11:10:11 -0300

    Sorting table added.

--- front/static/front/app.js
+++ front/static/front/app.js
@@ -1,21 +1,24 @@
-
 new Vue({ 
     el: '#app',
     data() {
         return {
-            api_url: 'api/', 
+            apiUrl: 'api/', 
+            
             tweet: {
                 name: '',
                 text: '',
             },
-            submitted: false,
-            tweets: null,
+            tweets: [],
+
+            currentKey: 'creation_date',
+            currentDirection: 'asc',
+
         }
     },
-    mounted() {
+    created() {
         var _this = this;
         $.ajax({
-            url: this.api_url,
+            url: this.apiUrl,
             method: 'GET',
             success: function (response) {
                 _this.tweets = response;
@@ -23,19 +26,33 @@ new Vue({
         });
     },
     methods: {
+        sortTweetsBy(key) {
+            if(key === this.currentKey) {
+              this.currentDirection = this.currentDirection === 'asc' ? 'desc' : 'asc';
+            }
+            this.currentKey = key;
+        },
         addTweet() {
-            var _this = this;
-
             $.ajax({
-                url: this.api_url,
+                url: this.apiUrl,
                 method: 'POST',
                 data: _this.tweet,
                 success: function (response) {
                     _this.tweets.unshift(response);
                 }
             });
-        },
-
-
-    }
-})
+        } 
+    },
+    computed: {
+        sortTweets() {
+            return this.tweets.sort((a,b) => {
+              let modifier = 1;
+              console.log(a, b);
+              if(this.currentDirection === 'desc') modifier = -1;
+              if(a[this.currentKey] < b[this.currentKey]) return -1 * modifier;
+              if(a[this.currentKey] > b[this.currentKey]) return 1 * modifier;
+              return 0;
+            });
+          }
+    },
+});
